bd3935a88acbca16e76d35aac82f1d287f5c4fa2
sandeep kumar <sandeepkumar.sk5@outlook.com>
2021-01-13 12:35:16 +0530

    Add _viewStart file in project

--- BookLibrary/BookLibrary/Repository/BookRepository.cs
+++ BookLibrary/BookLibrary/Repository/BookRepository.cs
@@ -24,11 +24,12 @@ namespace BookLibrary.Repository
         {
             return new List<BookModel>()
             {
-                new BookModel(){Id =1, Title="MVC", Author="Sandeep"},
-                new BookModel(){Id =2, Title="Dot Net Core", Author="Sandy"},
-                new BookModel(){Id =3, Title="C#", Author="Sanjay"},
-                new BookModel(){Id =4, Title="JAVA", Author="Sandeep"},
-                new BookModel(){Id =5, Title="PHP", Author="Sandy"},
+                new BookModel(){Id =1, Title="MVC", Author="Sandeep", Description="This is description for MVC book.", Category="Programing", Language="English", TotalPages=1206},
+                new BookModel(){Id =2, Title="Dot Net Core", Author="Sandy", Description="This is description for Dot Net Core book.", Category="Framwork", Language="Hindi", TotalPages=2206},
+                new BookModel(){Id =3, Title="C#", Author="Sanjay", Description="This is description for C# book.", Category="Developer", Language="Spanish", TotalPages=1002},
+                new BookModel(){Id =4, Title="JAVA", Author="Sandeep", Description="This is description for JAVA book.", Category="Programing", Language="Chines", TotalPages=506},
+                new BookModel(){Id =5, Title="PHP", Author="Sandy", Description="This is description for PHP book.", Category="Development", Language="French", TotalPages=906},
+                new BookModel(){Id =6, Title="Azure DevOps", Author="Adi", Description="This is description for Azure DevOps book.", Category="DevOps", Language="English", TotalPages=886},
             };
         }
     }
