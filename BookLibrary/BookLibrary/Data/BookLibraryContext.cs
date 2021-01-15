d49d6d9f07fed0b0c55ad2870949c5674a43e96f
sandeep kumar <sandeepkumar.sk5@outlook.com>
2021-01-15 13:01:36 +0530

    make databse connection

---
+++ BookLibrary/BookLibrary/Data/BookLibraryContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookLibrary.Data
+{
+    public class BookLibraryContext : DbContext 
+    {
+        public BookLibraryContext(DbContextOptions<BookLibraryContext> options)
+            : base(options)
+        {
+
+        }
+
+        public DbSet<Books> Books { get; set; }
+
+    }
+}
