660877e5b57dbc0c223b01ded742b0eb77ed9517
sandeep kumar <sandeepkumar.sk5@outlook.com>
2021-01-18 21:42:22 +0530

    Used Model validations

--- BookLibrary/BookLibrary/Models/BookModel.cs
+++ BookLibrary/BookLibrary/Models/BookModel.cs
@@ -2,23 +2,24 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
 namespace BookLibrary.Models
 {
     public class BookModel
     {
         public int Id { get; set; }
-
-        public string Title { get; set; }
-
+        [StringLength(100, MinimumLength =5)]
+        [Required(ErrorMessage ="Please enter the Title of your book!")]
+        public string Title { get; set; }       
+        [Display(Name ="Book Author Name")]
+        [Required(ErrorMessage ="Please enter the Author of book name!")]
         public string Author { get; set; }
-
+        [StringLength(100, MinimumLength =5)]
         public string Description { get; set; }
-
         public string Category { get; set; }
-
         public string Language { get; set; }
-
-        public int TotalPages { get; set; }
+        [Required(ErrorMessage = "Please enter the Total Pages!")]
+        public int? TotalPages { get; set; }
     }
 }
