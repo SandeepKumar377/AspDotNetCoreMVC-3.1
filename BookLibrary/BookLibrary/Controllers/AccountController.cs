bb542c055a0635b578960da43869eed781df049c
sandeep kumar <sandeepkumar.sk5@outlook.com>
2021-02-02 01:58:11 +0530

    added User sign up design page

---
+++ BookLibrary/BookLibrary/Controllers/AccountController.cs
@@ -0,0 +1,31 @@
+﻿using BookLibrary.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookLibrary.Controllers
+{
+    public class AccountController : Controller
+    {
+        [Route("signup")]
+        public IActionResult Signup()
+        {
+            return View();
+        }
+
+        [Route("signup")]
+        [HttpPost]
+        public IActionResult Signup(SignUpUserModel userModel)
+        {
+            if (ModelState.IsValid)
+            {
+                //write code
+
+                ModelState.Clear();
+            }
+            return View();
+        }
+    }
+}
---
+++ BookLibrary/BookLibrary/Models/SignUpUserModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookLibrary.Models
+{
+    public class SignUpUserModel
+    {
+        [Required(ErrorMessage ="Please enter your Email!")]
+        [Display(Name ="Email address")]
+        [EmailAddress(ErrorMessage ="Please enter a valid Email!")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Please enter your Password!")]
+        [Compare("ConfirmPassword", ErrorMessage ="Password does not match!")]
+        [Display(Name = "Password")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Required(ErrorMessage = "Please enter your Confirm Password!")]
+        [Display(Name = "Confirm Password")]
+        [DataType(DataType.Password)]
+        public string ConfirmPassword { get; set; }
+    }
+}
